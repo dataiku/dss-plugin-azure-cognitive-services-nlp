b8e5b7099be48e78ac90de4ae56154bfd6287b64
alexcombessie <alex.combessie@gmail.com>
2020-04-24 12:59:32 +0200

    continue refacto

--- custom-recipes/azure-cognitive-services-nlp-language-detection/recipe.py
+++ custom-recipes/azure-cognitive-services-nlp-language-detection/recipe.py
@@ -1,62 +1,75 @@
+# -*- coding: utf-8 -*-
 import logging
-import time
-import json
+from typing import List, Dict, AnyStr
+
+from retry import retry
+from ratelimit import limits, RateLimitException
+
 import dataiku
-from dataiku.customrecipe import *
-from dku_azure_cs import *
-from common import *
 
-#==============================================================================
+from plugin_io_utils import (
+    ErrorHandlingEnum, build_unique_column_names, validate_column_input)
+from api_parallelizer import api_parallelizer
+from dataiku.customrecipe import (
+    get_recipe_config, get_input_names_for_role, get_output_names_for_role)
+from cloud_api import (
+    API_SUPPORT_BATCH, APPLY_AXIS,
+    get_client, format_language_detection)
+
+
+# ==============================================================================
 # SETUP
-#==============================================================================
+# ==============================================================================
 
-logging.basicConfig(level=logging.INFO, format='[azure-cognitive-services plugin] %(levelname)s - %(message)s')
-
-connection_info = get_recipe_config().get('connection_info')
+api_configuration_preset = get_recipe_config().get("api_configuration_preset")
+api_quota_rate_limit = api_configuration_preset.get("api_quota_rate_limit")
+api_quota_period = api_configuration_preset.get("api_quota_period")
+parallel_workers = api_configuration_preset.get("parallel_workers")
+batch_size = api_configuration_preset.get("batch_size")
 text_column = get_recipe_config().get('text_column')
-output_format = get_recipe_config().get('output_format')
-should_output_raw_results = get_recipe_config().get('should_output_raw_results')
+error_handling = ErrorHandlingEnum[get_recipe_config().get('error_handling')]
 
 input_dataset_name = get_input_names_for_role('input_dataset')[0]
 input_dataset = dataiku.Dataset(input_dataset_name)
 input_schema = input_dataset.read_schema()
 input_columns_names = [col['name'] for col in input_schema]
-detected_language_column = generate_unique('detected_language', input_columns_names)
 
 output_dataset_name = get_output_names_for_role('output_dataset')[0]
 output_dataset = dataiku.Dataset(output_dataset_name)
 
-if text_column is None or len(text_column) == 0:
-    raise ValueError("You must specify the input text column")
-if text_column not in input_columns_names:
-    raise ValueError("Column '{}' is not present in the input dataset".format(text_column))
-
-#==============================================================================
-# RUN
-#==============================================================================
-
+validate_column_input(text_column, input_columns_names)
 input_df = input_dataset.get_dataframe()
-
-@with_original_indices
-def detect_language(text_list):
-    client = get_client(connection_info)
-    documents = [{"id": i, "text": text} for (i, text) in enumerate(text_list)]
-    logging.info("request: %d items / %d characters" % (len(text_list), sum([len(t) for t in text_list])))
-    start = time.time()
-    response = client.languages({"documents": documents})
-    logging.info("request took %.3fs" % (time.time() - start))
-    return response
+client = get_client(api_configuration_preset, "comprehend")
+column_prefix = "lang_detect_api"
+api_column_names = build_unique_column_names(input_df, column_prefix)
 
 
-for batch in run_by_batch(detect_language, input_df, text_column, batch_size=BATCH_SIZE, parallelism=PARALLELISM):
-    response, original_indices = batch
-    if response.get('errors'):
-        logging.error(response.get('errors'))
-    for i, raw_result in enumerate(response.get("documents")):
-        j = original_indices[i]
-        output = format_language_results(raw_result, output_format)
-        input_df.set_value(j, detected_language_column, output['detected_language'])
-        if should_output_raw_results:
-            input_df.set_value(j, 'raw_results', json.dumps(output['raw_results']))
+# ==============================================================================
+# RUN
+# ==============================================================================
 
-output_dataset.write_with_schema(input_df)
+
+@retry((RateLimitException, OSError), delay=api_quota_period, tries=5)
+@limits(calls=api_quota_rate_limit, period=api_quota_period)
+def call_api_language_detection(
+    batch: List[Dict], text_column: AnyStr
+) -> List[Dict]:
+    text_list = [str(r.get(text_column, '')).strip() for r in batch]
+    responses = client.batch_detect_dominant_language(TextList=text_list)
+    return responses
+
+
+output_df = api_parallelizer(
+    input_df=input_df, api_call_function=call_api_language_detection,
+    text_column=text_column, parallel_workers=parallel_workers,
+    api_support_batch=API_SUPPORT_BATCH, batch_size=batch_size,
+    error_handling=error_handling, column_prefix=column_prefix)
+
+logging.info("Formatting API results...")
+output_df = output_df.apply(
+   func=format_language_detection, axis=APPLY_AXIS,
+   response_column=api_column_names.response, error_handling=error_handling,
+   column_prefix=column_prefix)
+logging.info("Formatting API results: Done.")
+
+output_dataset.write_with_schema(output_df)
--- custom-recipes/azure-cognitive-services-nlp-named-entities-recognition/recipe.py
+++ custom-recipes/azure-cognitive-services-nlp-named-entities-recognition/recipe.py
@@ -1,62 +1,101 @@
+# -*- coding: utf-8 -*-
 import logging
-import time
 import json
+from typing import List, Dict, AnyStr, Union
+
+from ratelimit import limits, RateLimitException
+from retry import retry
+
 import dataiku
-from dataiku.customrecipe import *
-from dku_azure_cs import *
-from common import *
 
-#==============================================================================
+from plugin_io_utils import (
+    ErrorHandlingEnum, OutputFormatEnum,
+    build_unique_column_names, validate_column_input)
+from api_parallelizer import api_parallelizer
+from dataiku.customrecipe import (
+    get_recipe_config, get_input_names_for_role, get_output_names_for_role)
+from cloud_api import (
+    APPLY_AXIS, get_client, format_named_entity_recognition)
+
+
+# ==============================================================================
 # SETUP
-#==============================================================================
+# ==============================================================================
 
-logging.basicConfig(level=logging.INFO, format='[azure-cognitive-services plugin] %(levelname)s - %(message)s')
-
-connection_info = get_recipe_config().get('connection_info')
+api_configuration_preset = get_recipe_config().get("api_configuration_preset")
+api_quota_rate_limit = api_configuration_preset.get("api_quota_rate_limit")
+api_quota_period = api_configuration_preset.get("api_quota_period")
+parallel_workers = api_configuration_preset.get("parallel_workers")
+batch_size = api_configuration_preset.get("batch_size")
 text_column = get_recipe_config().get('text_column')
-language = get_recipe_config().get('language')
-should_output_raw_results = get_recipe_config().get('should_output_raw_results')
+text_language = get_recipe_config().get("language")
+language_column = get_recipe_config().get("language_column")
+output_format = OutputFormatEnum[get_recipe_config().get('output_format')]
+error_handling = ErrorHandlingEnum[get_recipe_config().get('error_handling')]
 
 input_dataset_name = get_input_names_for_role('input_dataset')[0]
 input_dataset = dataiku.Dataset(input_dataset_name)
 input_schema = input_dataset.read_schema()
 input_columns_names = [col['name'] for col in input_schema]
-entities_column = generate_unique('entities', input_columns_names)
 
 output_dataset_name = get_output_names_for_role('output_dataset')[0]
 output_dataset = dataiku.Dataset(output_dataset_name)
 
-if text_column is None or len(text_column) == 0:
-    raise ValueError("You must specify the input text column")
-if text_column not in input_columns_names:
-    raise ValueError("Column '{}' is not present in the input dataset".format(text_column))
+validate_column_input(text_column, input_columns_names)
 
-#==============================================================================
-# RUN
-#==============================================================================
+api_support_batch = True
+if text_language == "language_column":
+    api_support_batch = False
+    validate_column_input(language_column, input_columns_names)
 
 input_df = input_dataset.get_dataframe()
-
-@with_original_indices
-def detect_entities(text_list):
-    client = get_client(connection_info)
-    documents = [{"id": i, "text": text, "language": language} for (i, text) in enumerate(text_list)]
-    logging.info("request: %d items / %d characters" % (len(text_list), sum([len(t) for t in text_list])))
-    start = time.time()
-    response = client.entities({"documents": documents})
-    logging.info("request took %.3fs" % (time.time() - start))
-    return response
+client = get_client(api_configuration_preset, "comprehend")
+column_prefix = "ner_api"
+api_column_names = build_unique_column_names(input_df, column_prefix)
 
 
-for batch in run_by_batch(detect_entities, input_df, text_column, batch_size=BATCH_SIZE, parallelism=PARALLELISM):
-    response, original_indices = batch
-    if response.get('errors'):
-        logging.error(response.get('errors'))
-    for i, raw_result in enumerate(response.get("documents")):
-        j = original_indices[i]
-        output = format_entities_results(raw_result)
-        input_df.set_value(j, entities_column, json.dumps(output['entities']))
-        if should_output_raw_results:
-            input_df.set_value(j, 'raw_results', json.dumps(output['raw_results']))
+# ==============================================================================
+# RUN
+# ==============================================================================
 
-output_dataset.write_with_schema(input_df)
+
+@retry((RateLimitException, OSError), delay=api_quota_period, tries=5)
+@limits(calls=api_quota_rate_limit, period=api_quota_period)
+def call_api_named_entity_recognition(
+    text_column: AnyStr, text_language: AnyStr, language_column: AnyStr = None,
+    row: Dict = None, batch: List[Dict] = None,
+) -> List[Union[Dict, AnyStr]]:
+    if text_language == "language_column":
+        # Cannot use batch as language may be different for each row
+        text = row[text_column]
+        language_code = row[language_column]
+        empty_conditions = [
+            not(isinstance(text, str)), not(isinstance(language_code, str)),
+            str(text).strip() == '', str(language_code).strip() == '']
+        if any(empty_conditions):
+            return ''
+        response = client.detect_entities(
+            Text=text, LanguageCode=language_code)
+        return json.dumps(response)
+    else:
+        text_list = [str(r.get(text_column, '')).strip() for r in batch]
+        responses = client.batch_detect_entities(
+            TextList=text_list, LanguageCode=text_language)
+        return responses
+
+
+output_df = api_parallelizer(
+    input_df=input_df, api_call_function=call_api_named_entity_recognition,
+    text_column=text_column, text_language=text_language,
+    language_column=language_column, parallel_workers=parallel_workers,
+    api_support_batch=api_support_batch, batch_size=batch_size,
+    error_handling=error_handling, column_prefix=column_prefix)
+
+logging.info("Formatting API results...")
+output_df = output_df.apply(
+   func=format_named_entity_recognition, axis=APPLY_AXIS,
+   response_column=api_column_names.response, output_format=output_format,
+   column_prefix=column_prefix, error_handling=error_handling)
+logging.info("Formatting API results: Done.")
+
+output_dataset.write_with_schema(output_df)
